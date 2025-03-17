11aeaadf1a9de37cbfcd59b1d98de84b28ccbd2c
Anders Greve Sørensen <anders1709@gmail.com>
2025-03-17 12:26:43 +0100

    Create function to calculate angle of rotation

---
+++ imageRecognition/angleOfRotationCalculator.py
@@ -0,0 +1,7 @@
+import numpy as np
+import math
+
+def calculateAngleOfRotation(forwardsPoint, backwardsPoint, goalPoint):
+    middlePoint = ((forwardsPoint[0], backwardsPoint[0]) / 2, (forwardsPoint[1], backwardsPoint[1]) / 2)
+    angleOfRotation = math.atan2(goalPoint[1]-middlePoint[1], goalPoint[0]-middlePoint[0]) - math.atan2(forwardsPoint[1]-middlePoint[1], forwardsPoint[0] - middlePoint[0])
+    return angleOfRotation
