2adeec0ded0dcf0d18e2f091bb57d0cb8fe8e889
Simon Olsen <s235106@dtu.dk>
2025-03-17 13:11:57 +0100

    Main file made, just need to fix the detection

--- imageRecognition/robotColorDetection.py
+++ imageRecognition/robotColorDetection.py
@@ -1,97 +1,69 @@
 import cv2
 import numpy as np
 import imutils
+class RobotDetection:
+    def __init__(self):
+        # Adjusted Green color range
+        self.lowerGreen = (30, 40, 40)  
+        self.upperGreen = (90, 255, 255)
 
-cap = cv2.VideoCapture(2)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-if not cap.isOpened():
-    print("Error: Could not open webcam.")
-    exit()
-imgCounter = 0
-listOfBalls = []
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    
-    if not ret:
-        print("Failed to grab frame")
-        break
+        # Adjusted Blue color range
+        self.lowerBlue = (85, 40, 40)  
+        self.upperBlue = (135, 255, 255)
 
+    def RobotFrontDetection(self, frame):
+        frontRobotList = []
+        frame = cv2.resize(frame, (1280, 720))
+        blurred = cv2.GaussianBlur(frame, (9,9),0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
 
-    
-    # # Green color range
-    # lowerGreen = (35, 50, 50)  # Lower bound for green in HSV
-    # upperGreen = (85, 255, 255)  # Upper bound for green in HSV
-    
-    # # Blue color range
-    # lowerBlue = (90, 50, 50)  # Lower bound for blue in HSV
-    # upperBlue = (130, 255, 255)  # Upper bound for blue in HSV
-
-    # Adjusted Green color range
-    lowerGreen = (30, 40, 40)  
-    upperGreen = (90, 255, 255)
-
-    # Adjusted Blue color range
-    lowerBlue = (85, 40, 40)  
-    upperBlue = (135, 255, 255)
-
-    
-    frame = cv2.resize(frame, (1280, 720))
-    blurred = cv2.GaussianBlur(frame, (9,9),0)
-    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    maskGreen = cv2.inRange(hsv, lowerGreen, upperGreen)
-    maskBlue = cv2.inRange(hsv, lowerBlue, upperBlue)
-    
-    maskGreen = cv2.erode(maskGreen, None, iterations=2)
-    maskGreen = cv2.dilate(maskGreen, None, iterations=2)
-
-    maskBlue = cv2.erode(maskBlue, None, iterations=2)
-    maskBlue = cv2.dilate(maskBlue, None, iterations=2)
-
-    cntsFront, _ = cv2.findContours(maskGreen.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cntsBack, _ = cv2.findContours(maskBlue.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    if len(cntsFront) > 0:
-        for front in cntsFront:  # front is a single contour now
-            x, y, w, h = cv2.boundingRect(front)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        maskGreen = cv2.inRange(hsv, self.lowerGreen, self.upperGreen)
+        maskGreen = cv2.erode(maskGreen, None, iterations=2)
+        maskGreen = cv2.dilate(maskGreen, None, iterations=2)
+        cntsFront, _ = cv2.findContours(maskGreen.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-            # Display text label above the bounding box
-            # cv2.putText(frame, "Detected Color", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-    if len(cntsBack) > 0:
-        for back in cntsBack:
-            c, v, b, l = cv2.boundingRect(back)
-            
-            cv2.rectangle(frame, (c, v), (c + b, v + l), (255, 0, 0), 2)
-    # if len(cnts)> 0:
-    #     minRadius = 1
-    #     validContours = [c for c in cnts if cv2.minEnclosingCircle(c)[1] > minRadius]
-    #     for c in validContours:
-    #                     cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-    #                     cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        if len(cntsFront) > 0:
+            for front in cntsFront:
+                x, y, w, h = cv2.boundingRect(front)
+                M = cv2.moments(front)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                frontRobotList.append(center)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2) 
+                
+        return frontRobotList, 
+
+    def BackRobotDetection(self, frame):
+        backRobotList = []
+        frame = cv2.resize(frame, (1280, 720))
+        blurred = cv2.GaussianBlur(frame, (9,9),0)
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        maskBlue = cv2.inRange(hsv, self.lowerBlue, self.upperBlue)
+        
 
 
+        maskBlue = cv2.erode(maskBlue, None, iterations=2)
+        maskBlue = cv2.dilate(maskBlue, None, iterations=2)
+        cntsBack, _ = cv2.findContours(maskBlue.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 
-    # Display the resulting frame
-    cv2.imshow('Webcam Feed', frame)
+        if len(cntsBack) > 0:
+            for back in cntsBack:  # front is a single contour now
+                x, y, w, h = cv2.boundingRect(back)
+                M = cv2.moments(back)
+                center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+                backRobotList.append(center)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    key = cv2.waitKey(1)
 
-    # Press 'q' to exit
-    if key & 0xFF == ord('q'):
-        break
+                # Display text label above the bounding box
+                # cv2.putText(frame, "Detected Color", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
 
-    elif key & 0xFF == ord('s'):
-        imgName = "data/opencv_frame_{}.png".format(imgCounter)
-        cv2.imwrite(imgName, frame)
-        print("Screenshot taken")
-        imgCounter += 1
-
-# Release the webcam and close the window
-cap.release()
-cv2.destroyAllWindows()
-
+        # if len(cnts)> 0:
+        #     minRadius = 1
+        #     validContours = [c for c in cnts if cv2.minEnclosingCircle(c)[1] > minRadius]
+        #     for c in validContours:
+        #                     cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+        #                     cv2.circle(frame, center, 5, (0, 0, 255), -1)
+        return backRobotList
---
+++ main.py
@@ -0,0 +1,47 @@
+import cv2
+from imageRecognition.ballColorDetection import BallDetection
+from imageRecognition.robotColorDetection import RobotDetection
+
+def main():
+    cap = cv2.VideoCapture(2)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+    
+    if not cap.isOpened():
+        print("Error: Could not open webcam.")
+        return
+    
+    ball_detector = BallDetection()
+    robot_detector = RobotDetection()  
+    # robot_detector = RobotColorDetection
+
+    while True:
+        ret, frame = cap.read()
+        
+        if not ret:
+            print("Failed to grab frame")
+            break
+        
+        orangeballs = ball_detector.DetectOrange(frame) 
+        whiteballs = ball_detector.DetectWhite(frame) 
+        frontrobots = robot_detector.RobotFrontDetection(frame) 
+        backrobots = robot_detector.BackRobotDetection(frame) 
+
+        for ball in orangeballs:
+            cv2.circle(frame, ball, 10, (0, 165, 255), -1)  # Draw for each orange ball 
+
+        print("Orange Balls detected:", orangeballs)
+        print("White Balls detected:", whiteballs)
+        print("Front Robots detected:", frontrobots)
+        print("Back Robots detected:", backrobots)
+        
+        cv2.imshow("Frame", frame)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):  # Press 'q' to exit
+            break
+    
+    cap.release()
+    cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
