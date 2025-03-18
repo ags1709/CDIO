e8267bd11f1776ccbf29385ecd208521491541b3
Anders Greve Sørensen <anders1709@gmail.com>
2025-03-18 07:33:18 +0100

    Setup initial communication with robot

---
+++ imageRecognition/sendData.py
@@ -0,0 +1,10 @@
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("192.168.138.21", 12352))  # Connect to server
+inputuser = ''
+while(inputuser != 'q'):
+    inputuser = input("command\n")
+    client_socket.sendall(inputuser.encode())
+
+client_socket.close() 
