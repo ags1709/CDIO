954b5a52f172056966f297c1495577bb6efd7cd5
Anders Greve Sørensen <anders1709@gmail.com>
2025-03-20 14:00:03 +0100

    Change socket number

--- imageRecognition/sendData.py
+++ imageRecognition/sendData.py
@@ -1,7 +1,7 @@
 import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("192.168.138.143", 12352))  # Connect to server
+client_socket.connect(("192.168.138.143", 12353))  # Connect to server
 inputuser = ''
 while(inputuser != 'q'):
     inputuser = input("command\n")
