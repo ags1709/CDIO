706d58ccc9716e1a4c89ea56e9ec7e29f5daa7b1
Anders Greve Soerensen <anders1709@gmail.com>
2025-03-23 20:51:28 +0100

    Slightly change values in movementController

--- movementController.py
+++ movementController.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def calculateSpeedAndRotation(distanceFromBall, angleToBall):
     # Proportionality constants. Tune to change how fast speed changes
-    kp_speed = 2
-    kp_turn = 1
+    kp_speed = 0.1
+    kp_turn = 50
 
-    targetDistanceFromBall = 5
+    targetDistanceFromBall = 20
     targetAngleToBall = 0
 
     # Assuming that we use MoveSteering().on(steering, speed), the values range from -100 to 100, adjust below values accordingly
-    forwardSpeed = max(2, min(100, kp_speed * (distanceFromBall - targetDistanceFromBall)))
+    forwardSpeed = max(5, min(100, kp_speed * (distanceFromBall - targetDistanceFromBall)))
     turnSpeed = max(-50, min(50, kp_turn * (angleToBall - targetAngleToBall))) 
 
     return (turnSpeed, forwardSpeed)
