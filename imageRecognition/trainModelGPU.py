706b6c29dd9c905d92164f92ca4f20f64106ee12
Anders Greve Sørensen <anders1709@gmail.com>
2025-03-23 13:50:58 +0100

    Modify trainModel file to just run checks

--- imageRecognition/trainModelGPU.py
+++ imageRecognition/trainModelGPU.py
@@ -15,39 +15,37 @@ import torch
 from ultralytics import YOLO
 model = YOLO('yolov8m.pt')
 
+print(torch.cuda.is_available())
+
 # if torch.cuda.is_available() and torch.cuda.device_count() > 1:
 #     print(f"Training on {torch.cuda.device_count()} GPUs.")
 #     # Wrap the model with DataParallel
 #     model = torch.nn.DataParallel(model)  # Automatically uses all available GPUs
 
 # Move model to GPUs
-model = model.cuda()
+# model = model.cuda()
 
-dloc = f"{HOME}/pingpong/"
+# dloc = f"{HOME}/robotRecognitionData/PublicColoredShapes/"
 
 # Train
 
-print("Training")
+# print("Training")
 
-train_config = {
-    'epochs': 25,  # Number of training epochs
-    'batch': 8,  # Batch size
-    'imgsz': 800,  # Input image size for training
-    #'device': 'cuda' if torch.cuda.is_available() else 'cpu',  # Use GPU if available
-    'device': '0,1',
-    'data': f"{dloc}data.yaml",  # Path to dataset YAML file
-    'project': 'runs/train',  # Path to save training results
-    'name': 'experiment',  # Name of the experiment
-    'save_period': 10,  # Save checkpoint every 'n' epochs
-    'verbose': True  # Enable verbose output
-}
+# train_config = {
+#     'epochs': 25,  # Number of training epochs
+#     'batch': 8,  # Batch size
+#     'imgsz': 800,  # Input image size for training
+#     #'device': 'cuda' if torch.cuda.is_available() else 'cpu',  # Use GPU if available
+#     'device': '0,1',
+#     'data': f"{dloc}data.yaml",  # Path to dataset YAML file
+#     'project': 'runs/train',  # Path to save training results
+#     'name': 'experiment',  # Name of the experiment
+#     'save_period': 10,  # Save checkpoint every 'n' epochs
+#     'verbose': True  # Enable verbose output
+# }
 
 
-model.train(**train_config)
-#results = model.train(data=f"{dloc}data.yaml", epochs=25, batch_size=80)
-model.save('yolov8_multi_gpu.pt')
-#os.system(f"yolo task=detect mode=train model=yolov8l.pt data={dloc}data.yaml epochs=50 imgsz=800 plots=True device=cpu")
+# model.train(**train_config)
 
-# Detecting
-#print("Detecting")
-#os.system(f"yolo task=detect mode=predict model={HOME}/runs/detect/train1/weights/best.pt conf=0.5 source={dloc}/test/images save=True")
+# Change the below name to a suitable name for the model
+# model.save('yolov8mRobotDetection.pt')
