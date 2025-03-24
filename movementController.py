076a8c6c38566f4db182e445a7b0bb9fabb3f70e
Anders Greve Soerensen <anders1709@gmail.com>
2025-03-24 18:48:18 +0100

    Change angle calculation and normalize it to (-π, π]

--- movementController.py
+++ movementController.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def calculateSpeedAndRotation(distanceFromBall, angleToBall):
     # Proportionality constants. Tune to change how fast speed changes
-    kp_speed = 0.1
+    kp_speed = 0.2
     kp_turn = 50
 
     targetDistanceFromBall = 20
@@ -14,7 +14,7 @@ def calculateSpeedAndRotation(distanceFromBall, angleToBall):
 
     # Assuming that we use MoveSteering().on(steering, speed), the values range from -100 to 100, adjust below values accordingly
     forwardSpeed = max(5, min(100, kp_speed * (distanceFromBall - targetDistanceFromBall)))
-    turnSpeed = max(-50, min(50, kp_turn * (angleToBall - targetAngleToBall))) 
+    turnSpeed = max(-100, min(100, kp_turn * (angleToBall - targetAngleToBall))) 
 
     return (turnSpeed, forwardSpeed)
     
