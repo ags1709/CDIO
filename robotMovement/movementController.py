44849fbdb3452c7cb42b879d1cc9334dba7dd4e3
Anders Greve Sørensen <anders1709@gmail.com>
2025-04-28 17:58:04 +0200

    Add finite state machine to control robots behaviour more accurately

--- robotMovement/movementController.py
+++ robotMovement/movementController.py
@@ -1,20 +1,44 @@
 import numpy as np
 
-
-def calculateSpeedAndRotation(distanceFromBall, angleToBall):
-    # Proportionality constants. Tune to change how fast speed changes
-    kp_speed = 0.2
-    kp_turn = 100
-
-    targetDistanceFromBall = 20
-    targetAngleToBall = 0
-
-    if distanceFromBall == None or angleToBall == None:
+# PID controller
+def calculateSpeedAndRotation(distanceFromTarget, angleToTarget, state):
+    if distanceFromTarget == None or angleToTarget == None:
         return (0, 0)
+    # Robot should drive differently depending on state.
+    if state == "SEARCH_BALLS":
+        # Proportionality constants. Tune to change how fast speed changes
+        kp_speed = 0.2
+        kp_turn = 200
 
-    # Assuming that we use MoveSteering().on(steering, speed), the values range from -100 to 100, adjust below values accordingly
-    forwardSpeed = max(5, min(100, kp_speed * (distanceFromBall - targetDistanceFromBall)))
-    turnSpeed = max(-100, min(100, kp_turn * (angleToBall - targetAngleToBall))) 
+        goalDistanceFromBall = 20
+        goalAngleToBall = 0
+        # Assuming that we use MoveSteering().on(steering, speed), the values range from -100 to 100, adjust below values accordingly
+        forwardSpeed = max(5, min(100, kp_speed * (distanceFromTarget - goalDistanceFromBall)))
+        turnSpeed = max(-100, min(100, kp_turn * (angleToTarget - goalAngleToBall))) 
+
+    elif state == "TO_INTERMEDIARY":
+        kp_speed = 0.2
+        kp_turn = 200
+
+        goalDistanceFromBall = 10
+        goalAngleToBall = 0
+
+        forwardSpeed = max(40, min(100, kp_speed * (distanceFromTarget - goalDistanceFromBall)))
+        turnSpeed = max(-100, min(100, kp_turn * (angleToTarget - goalAngleToBall))) 
+
+    elif state == "TO_GOAL":
+        kp_speed = 0.2
+        kp_turn = 200
+
+        goalDistanceFromBall = 100
+        goalAngleToBall = 0
+
+        forwardSpeed = max(0, min(100, kp_speed * (distanceFromTarget - goalDistanceFromBall)))
+        turnSpeed = max(-100, min(100, kp_turn * (angleToTarget - goalAngleToBall))) 
+    
+    
+
+
 
     return (turnSpeed, forwardSpeed)
     
--- testMain.py
+++ testMain.py
@@ -14,7 +14,15 @@ def main():
     # client_socket.connect(("192.168.137.73", 12358))
 
     od = ObjectDetection("imageRecognition/yolov8_20250424.pt", 2)
-    intermediaryPointReached = False
+    
+    # States for state machine
+    SEARCH_BALLS = "SEARCH_BALLS"
+    TO_INTERMEDIARY = "TO_INTERMEDIARY"
+    TO_GOAL = "TO_GOAL"
+    
+    state = SEARCH_BALLS
+    # intermediaryPointReached = False
+
     # Main loop. Runs entire competition program.
     while True:
         # use model to detect objects
@@ -28,39 +36,46 @@ def main():
         vomit = False
         targetBall = None
 
-        if detectedObjects.get("whiteBalls") and len(detectedObjects["whiteBalls"]) > 0:
-            targetBall = min(detectedObjects["whiteBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
-        elif detectedObjects.get("orangeBalls") and len(detectedObjects["orangeBalls"]) > 0:
-            targetBall = min(detectedObjects["orangeBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
+        # Use state machine
+        if state == SEARCH_BALLS:
+            if detectedObjects.get("whiteBalls") and len(detectedObjects["whiteBalls"]) > 0:
+                targetBall = min(detectedObjects["whiteBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
+            elif detectedObjects.get("orangeBalls") and len(detectedObjects["orangeBalls"]) > 0:
+                targetBall = min(detectedObjects["orangeBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
 
-        # Calculate distance and angle to the selected ball
-        if targetBall and robotPos[0] is not None and robotPos[1] is not None:
-            robotDistance = calculateDistance(robotPos[0], targetBall)
-            robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], targetBall)
+            # Calculate distance and angle to the selected ball
+            if targetBall and robotPos[0] is not None and robotPos[1] is not None:
+                robotDistance = calculateDistance(robotPos[0], targetBall)
+                robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], targetBall)
 
-        # If no balls are present, move to goal and turn in balls.
-        # NOTE: This turns in balls in the small goal. This assumes that the small goal is on the right side of the camera
-        if not detectedObjects["whiteBalls"] and not detectedObjects["orangeBalls"]:
-            if detectedObjects["playfield"]:
-                intermediaryPoint = (detectedObjects["goals"][1][0] - 300, detectedObjects["goals"][1][1])
+            # If no balls are present, move to intermediary point in preperation for turning in balls.
+            if not detectedObjects["whiteBalls"] and not detectedObjects["orangeBalls"]:
+                state = TO_INTERMEDIARY
 
-            if not intermediaryPointReached:
-                robotMiddle = ((robotPos[0][0] + robotPos[1][0]) / 2, (robotPos[0][1] + robotPos[1][1]) / 2)
-                robotDistance = calculateDistance(robotMiddle, intermediaryPoint)
-                robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], intermediaryPoint)    
+        elif state == TO_INTERMEDIARY:
+            intermediaryPoint = (detectedObjects["goals"][1][0] - 300, detectedObjects["goals"][1][1])
+            # Have robots middle point reach the intermediary point as it makes for better arrival at goal.
+            robotMiddle = ((robotPos[0][0] + robotPos[1][0]) / 2, (robotPos[0][1] + robotPos[1][1]) / 2)
 
-                if robotDistance <= 10 and -0.2 < robotAngle < 0.2:
-                    intermediaryPointReached = True
+            robotDistance = calculateDistance(robotMiddle, intermediaryPoint)
+            robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], intermediaryPoint)    
 
-            else:
-                robotDistance = calculateDistance(robotPos[0], detectedObjects["goals"][1]) - 50
-                robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], detectedObjects["goals"][1])
-                if robotDistance <= 100 and robotAngle < 0.2 and robotAngle > -0.2:
-                    vomit = True
-                else: 
-                    vomit = False
+            if robotDistance <= 10 and -0.2 < robotAngle < 0.2:
+                state = TO_GOAL
+                # intermediaryPointReached = True
         
-        robotMovement = calculateSpeedAndRotation(robotDistance, robotAngle)
+        elif state == TO_GOAL:
+            # NOTE: This turns in balls in the small goal. This assumes that the small goal is on the right side of the camera
+            goalPos = detectedObjects["goals"][1]
+
+            robotDistance = calculateDistance(robotPos[0], goalPos)
+            robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], goalPos)
+            if robotDistance <= 100 and robotAngle < 0.2 and robotAngle > -0.2:
+                vomit = True
+            else: 
+                vomit = False
+        
+        robotMovement = calculateSpeedAndRotation(robotDistance, robotAngle, state)
 
         # Send data to robot
         # client_socket.sendall(f"{round(robotMovement[0])}#{round(robotMovement[1])}#False#{vomit}\n".encode())
