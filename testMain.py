4f5264ea0a6bbdd0627f58a1ed7d21626aeae7b5
Anders Greve Sørensen <anders1709@gmail.com>
2025-04-28 13:53:12 +0200

    Navigate to intermediary point before goal

--- testMain.py
+++ testMain.py
@@ -10,11 +10,11 @@ from imageRecognition.detect import ObjectDetection
 
 def main():
     # Set connection to robot
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(("192.168.137.205", 12358))
+    # client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # client_socket.connect(("192.168.137.73", 12358))
 
     od = ObjectDetection("imageRecognition/yolov8_20250424.pt", 2)
-
+    intermediaryPointReached = False
     # Main loop. Runs entire competition program.
     while True:
         # use model to detect objects
@@ -22,7 +22,6 @@ def main():
         
         # This is the two points used to identify the robots position. 
         robotPos = calculateRobotPositionFlexible(detectedObjects["frontLeftCorner"], detectedObjects["frontRightCorner"], detectedObjects["backLeftCorner"], detectedObjects["backRightCorner"])
-        print(f"Detected objects: {detectedObjects}")
 
         robotDistance = None
         robotAngle = None
@@ -31,28 +30,40 @@ def main():
 
         if detectedObjects.get("whiteBalls") and len(detectedObjects["whiteBalls"]) > 0:
             targetBall = min(detectedObjects["whiteBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
-            print("Targeting NEAREST WHITE ball")
         elif detectedObjects.get("orangeBalls") and len(detectedObjects["orangeBalls"]) > 0:
             targetBall = min(detectedObjects["orangeBalls"], key=lambda ball: calculateDistance(robotPos[0], ball))
-            print("No white balls found. Targeting NEAREST ORANGE ball.")
 
         # Calculate distance and angle to the selected ball
         if targetBall and robotPos[0] is not None and robotPos[1] is not None:
             robotDistance = calculateDistance(robotPos[0], targetBall)
             robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], targetBall)
 
+        # If no balls are present, move to goal and turn in balls.
+        # NOTE: This turns in balls in the small goal. This assumes that the small goal is on the right side of the camera
         if not detectedObjects["whiteBalls"] and not detectedObjects["orangeBalls"]:
-            robotDistance = calculateDistance(robotPos[0], detectedObjects["goals"][1])
-            robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], detectedObjects["goals"][1])
-            if robotDistance <= 100 and robotAngle < 0.2 and robotAngle > -0.2:
-                vomit = True
-            else: 
-                vomit = False
+            if detectedObjects["playfield"]:
+                intermediaryPoint = (detectedObjects["goals"][1][0] - 300, detectedObjects["goals"][1][1])
+
+            if not intermediaryPointReached:
+                robotMiddle = ((robotPos[0][0] + robotPos[1][0]) / 2, (robotPos[0][1] + robotPos[1][1]) / 2)
+                robotDistance = calculateDistance(robotMiddle, intermediaryPoint)
+                robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], intermediaryPoint)    
+
+                if robotDistance <= 10 and -0.2 < robotAngle < 0.2:
+                    intermediaryPointReached = True
+
+            else:
+                robotDistance = calculateDistance(robotPos[0], detectedObjects["goals"][1]) - 50
+                robotAngle = calculateAngleOfRotation(robotPos[0], robotPos[1], detectedObjects["goals"][1])
+                if robotDistance <= 100 and robotAngle < 0.2 and robotAngle > -0.2:
+                    vomit = True
+                else: 
+                    vomit = False
         
         robotMovement = calculateSpeedAndRotation(robotDistance, robotAngle)
 
         # Send data to robot
-        client_socket.sendall(f"{round(robotMovement[0])}#{round(robotMovement[1])}#False#{vomit}\n".encode())
+        # client_socket.sendall(f"{round(robotMovement[0])}#{round(robotMovement[1])}#False#{vomit}\n".encode())
 
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
