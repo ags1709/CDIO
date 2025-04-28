8c39e0dcc86a9d646fa426e289e28d80d74b883e
Anders Greve Soerensen <anders1709@gmail.com>
2025-04-28 21:43:50 +0200

    Make obstacle avoidance class, and setup a bit more structure

--- robotMovement/obstacleAvoidance.py
+++ robotMovement/obstacleAvoidance.py
@@ -1,11 +1,34 @@
 import numpy as np
 
-GRID_WIDTH = 20
-GRID_HEIGHT = 20
-GRID_RESOLUTION = 10
 
-def toGridCoords(x, y):
-    return (x // GRID_RESOLUTION, y // GRID_RESOLUTION)
+class obstacleAvoidance:
+    # FieldPixelSize is expected in (width, height)
+    def __init__(self, fieldPixelSize):
+        # Resolution of grid. The smaller this is the better the accuracy, but the more computation power required.
+        self. GRID_RESOLUTION = 10
+        self.GRID_WIDTH = fieldPixelSize[0] // self.GRID_RESOLUTION
+        self. GRID_HEIGHT = fieldPixelSize[1] // self.GRID_RESOLUTION
 
-def toPixelCoords(gridX, gridY):
-    return (gridX * GRID_RESOLUTION, gridY * GRID_RESOLUTION)
+    def toGridCoords(self, x, y):
+        return (x // self.GRID_RESOLUTION, y // self.GRID_RESOLUTION)
+    
+    def toPixelCoords(self, gridX, gridY):
+        return (gridX * self.GRID_RESOLUTION, gridY * self.GRID_RESOLUTION)
+    
+    def heuristic(self, a, b):
+        # probably euclidean or manhatten distance.
+        # Takes grid points as input.
+        pass
+
+    def astar(self, start, goal):
+        # path finding from start to goal on the grid
+        pass
+
+
+    
+# Testing
+OA = obstacleAvoidance((847, 1549))
+pixelPoint = (309, 823)
+gridPoint = OA.toGridCoords(pixelPoint[0], pixelPoint[1])
+
+print(gridPoint)
