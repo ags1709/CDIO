87a3eeb67ccdda578b467c32668f0a77fff914b9
Anders Greve Sørensen <anders1709@gmail.com>
2025-05-01 11:52:43 +0200

    Change structure of obstacle avoidance

--- robotMovement/obstacleAvoidance.py
+++ robotMovement/obstacleAvoidance.py
@@ -1,34 +1,45 @@
 import numpy as np
+from robotMovement.distanceBetweenObjects import calculateDistance
 
-
-class obstacleAvoidance:
-    # FieldPixelSize is expected in (width, height)
-    def __init__(self, fieldPixelSize):
-        # Resolution of grid. The smaller this is the better the accuracy, but the more computation power required.
-        self. GRID_RESOLUTION = 10
-        self.GRID_WIDTH = fieldPixelSize[0] // self.GRID_RESOLUTION
-        self. GRID_HEIGHT = fieldPixelSize[1] // self.GRID_RESOLUTION
-
-    def toGridCoords(self, x, y):
-        return (x // self.GRID_RESOLUTION, y // self.GRID_RESOLUTION)
+def toGridCoords(self, x, y, grid_resolution):
+        return (x // grid_resolution, y // grid_resolution)
     
-    def toPixelCoords(self, gridX, gridY):
-        return (gridX * self.GRID_RESOLUTION, gridY * self.GRID_RESOLUTION)
+def toPixelCoords(self, gridX, gridY, grid_resolution):
+    return (gridX * grid_resolution, gridY * grid_resolution)
     
-    def heuristic(self, a, b):
-        # probably euclidean or manhatten distance.
-        # Takes grid points as input.
-        pass
+def heuristic(self, a, b):
+    # probably euclidean or manhatten distance.
+    # Takes grid points as input.
+    
+    pixelA = toPixelCoords(a)
+    pixelB = toPixelCoords(b)
+
+    euclideanDistance = calculateDistance(pixelA, pixelB)
+    return euclideanDistance
+
+
+def returnPath(startingPoint, endingPoint, fieldPixelSize, detectedObstacles):
+    # Calculates a grid path from starting point to ending point
+    # Resolution of grid. The smaller this is the better the accuracy, but the more computation power required.
+    GRID_RESOLUTION = 10
+    GRID_WIDTH = fieldPixelSize[0] // GRID_RESOLUTION
+    GRID_HEIGHT = fieldPixelSize[1] // GRID_RESOLUTION
+
+    grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+
+    for obstacle in detectedObstacles:
+        # for each obstacle figure out all grid points it overlaps and set them to 1.
+
+        # obstacleGridPos = toGridCoords(obstacle[0], obstacle[1])
+        grid[obstacleGridPos[1]][obstacleGridPos[0]] = 1
+
+    
+
+
+    
 
     def astar(self, start, goal):
         # path finding from start to goal on the grid
         pass
 
 
-    
-# Testing
-OA = obstacleAvoidance((847, 1549))
-pixelPoint = (309, 823)
-gridPoint = OA.toGridCoords(pixelPoint[0], pixelPoint[1])
-
-print(gridPoint)
