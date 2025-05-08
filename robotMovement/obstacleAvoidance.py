6b65f266af8db53cedc18bd67512acd15a00f76a
Anders Greve Sørensen <anders1709@gmail.com>
2025-05-08 11:44:27 +0200

    Implement function to calculate grid and mark obstacles in grid

--- robotMovement/obstacleAvoidance.py
+++ robotMovement/obstacleAvoidance.py
@@ -18,30 +18,50 @@ def heuristic(self, a, b):
     return euclideanDistance
 
 
-def returnPath(startingPoint, endingPoint, fieldPixelSize, detectedObstacles):
+def calculateMarkedGrid(fieldPixelSize, detectedObstacles):
     # Calculates a grid path from starting point to ending point
     # Resolution of grid. The smaller this is the better the accuracy, but the more computation power required.
-    GRID_RESOLUTION = 10
-    GRID_WIDTH = fieldPixelSize[0] // GRID_RESOLUTION
-    GRID_HEIGHT = fieldPixelSize[1] // GRID_RESOLUTION
+    gridResolution = 10
+    fieldWidthPx, fieldHeightPx = fieldPixelSize
+    # Width of grid in nr of cells
+    gridWidth = fieldWidthPx // gridResolution
+    # Height of grid in nr of cells
+    gridHeight = fieldHeightPx // gridResolution
 
-    grid = [[0 for _ in range(GRID_WIDTH)] for _ in range(GRID_HEIGHT)]
+    grid = [[0 for _ in range(gridWidth)] for _ in range(gridHeight)]
 
+    cellWidthPx = fieldPixelSize[0] / gridWidth 
+    cellHeightPx = fieldPixelSize[1] / gridHeight
+    # Assume each obstacle is a list or tuple of two points, each point being a tuple.
     for obstacle in detectedObstacles:
         # for each obstacle figure out all grid points it overlaps and set them to 1.
         xMin, yMin = obstacle[0]
         xMax, yMax = obstacle[1]
-        colStart = int(xMin / GRID_WIDTH) 
-        colEnd = int(xMax / GRID_WIDTH)
-        rowStart = int(yMin / GRID_HEIGHT)
-        rowEnd = int(yMax / GRID_HEIGHT)
 
-        # obstacleGridPos = toGridCoords(obstacle[0], obstacle[1])
-        # grid[obstacleGridPos[1]][obstacleGridPos[0]] = 1
+        # Make sure min <= max
+        # Unsure if this is neccessary
+        xMin, xMax = sorted((xMin, xMax))
+        yMin, yMax = sorted((yMin, yMax))
+
+        # Calculate where obstacles begin and end in grid coordinates
+        colStart = int(xMin / cellWidthPx) 
+        colEnd = int(xMax / cellWidthPx)
+        rowStart = int(yMin / cellHeightPx)
+        rowEnd = int(yMax / cellHeightPx)
+
+        # Clamp values to avoid out of bounds
+        colStart = max(0, min(gridWidth - 1, colStart))
+        colEnd = max(0, min(gridWidth - 1, colEnd))
+        rowStart = max(0, min(gridHeight - 1, rowStart))
+        rowEnd = max(0, min(gridWidth - 1, rowEnd))
+
+        for row in range(rowStart, rowEnd + 1):
+             for col in range(colStart, colEnd + 1):
+                  grid[row][col] = 1
+
+    return grid
 
     
-
-
     
 
     def astar(self, start, goal):
