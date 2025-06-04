83d1b2ebf5e998e854b6d514c6b04ecefacfaf15
Anders Greve Sørensen <anders1709@gmail.com>
2025-06-04 13:19:08 +0200

    Fix test of obstacle avoidance grid calculator

--- robotMovement/obstacleAvoidance.py
+++ robotMovement/obstacleAvoidance.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 # from robotMovement.distanceBetweenObjects import calculateDistance
 
 def toGridCoords(self, x, y, grid_resolution):
@@ -23,7 +24,7 @@ def calculateMarkedGrid(fieldPixelSize, detectedObstacles):
     # Calculates a grid path from starting point to ending point
     # Resolution of grid. The smaller this is the better the accuracy, but the more computation power required.
     gridResolution = 10
-    fieldWidthPx, fieldHeightPx = fieldPixelSize
+    fieldWidthPx, fieldHeightPx = fieldPixelSize    
     # Width of grid in nr of cells
     gridWidth = fieldWidthPx // gridResolution
     # Height of grid in nr of cells
@@ -31,35 +32,23 @@ def calculateMarkedGrid(fieldPixelSize, detectedObstacles):
 
     grid = [[0 for _ in range(gridWidth)] for _ in range(gridHeight)]
 
-    cellWidthPx = fieldPixelSize[0] / gridWidth 
-    cellHeightPx = fieldPixelSize[1] / gridHeight
+    cellWidthPx = fieldWidthPx / gridWidth 
+    cellHeightPx = fieldHeightPx / gridHeight
     # Assume each obstacle is a list or tuple of two points, each point being a tuple.
     for obstacle in detectedObstacles:
         # for each obstacle figure out all grid points it overlaps and set them to 1.
+        
         xMin, yMin = obstacle[0]
         xMax, yMax = obstacle[1]
 
         # Make sure min <= max
-        # Unsure if this is neccessary
         xMin, xMax = sorted((xMin, xMax))
         yMin, yMax = sorted((yMin, yMax))
 
-        # # Calculate where obstacles begin and end in grid coordinates
-        # colStart = int(xMin / cellWidthPx) 
-        # colEnd = int(xMax / cellWidthPx)
-        # rowStart = int(yMin / cellHeightPx)
-        # rowEnd = int(yMax / cellHeightPx)
-
-        # # Clamp values to avoid out of bounds
-        # colStart = max(0, min(gridWidth - 1, colStart))
-        # colEnd = max(0, min(gridWidth - 1, colEnd))
-        # rowStart = max(0, min(gridHeight - 1, rowStart))
-        # rowEnd = max(0, min(gridWidth - 1, rowEnd))
-
-        colStart = max(0, min(gridWidth - 1, int(xMin / cellWidthPx)))
-        colEnd =   max(0, min(gridWidth - 1, int((xMax - 1) / cellWidthPx)))
-        rowStart = max(0, min(gridHeight - 1, int(yMin / cellHeightPx)))
-        rowEnd =   max(0, min(gridHeight - 1, int((yMax - 1) / cellHeightPx)))
+        colStart = max(0, min(gridWidth - 1, int(math.floor(xMin / cellWidthPx))))
+        colEnd   = max(0, min(gridWidth - 1, int(math.ceil(xMax / cellWidthPx) - 1)))
+        rowStart = max(0, min(gridHeight - 1, int(math.floor(yMin / cellHeightPx))))
+        rowEnd   = max(0, min(gridHeight - 1, int(math.ceil(yMax / cellHeightPx) - 1)))
 
         for row in range(rowStart, rowEnd + 1):
              for col in range(colStart, colEnd + 1):
@@ -71,18 +60,22 @@ def astar(self, start, goal):
     # path finding from start to goal on the grid
     pass
 
+
+
 testFieldPixelSize = (200, 100)
 testGridResolution = 10
 testFieldWidthPx, testFieldHeightPx = testFieldPixelSize
 testGridWidth = testFieldWidthPx // testGridResolution
 testGridHeight = testFieldHeightPx // testGridResolution
 testGridBeforeObstacles = np.array([[0 for _ in range(testGridWidth)] for _ in range(testGridHeight)])
-testObstacles = [((20, 20), (30, 30)), ((140, 120), (180, 190))]
+testObstacles = [((20, 20), (30, 30)), ((140, 50), (180, 100))]
 testGridObstaclesMarked = calculateMarkedGrid(testFieldPixelSize, testObstacles)
+
 
 print(f"Grid before:\n {testGridBeforeObstacles}")
 print()
 print(f"Grid With Obstacles:\n {testGridObstaclesMarked}")
+
 
     
 
