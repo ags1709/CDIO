a6c5e296271dc43f8f55da2b777ac0c9d0a887d3
Anders Greve Soerensen <anders1709@gmail.com>
2025-06-06 23:10:22 +0200

    Make small adjustments to movement and add comments for future work and testing

--- robotMovement/movementController.py
+++ robotMovement/movementController.py
@@ -1,7 +1,14 @@
 import numpy as np
 
 def getTurnSpeed(angleToTarget: float):
-    return max(-100, min(100, angleToTarget**3*40+angleToTarget*50)) # x^(3)*40+x*50
+    # return max(-100, min(100, angleToTarget**3*40+angleToTarget*50)) # x^(3)*40+x*50
+    
+    # Try tuning constant.
+    kpTurn = 20
+    turnSpeed = kpTurn * angleToTarget
+
+    # Try capping turn speed at eg. 50. Maybe not, as this would not allow for turning in place. Test?
+    # return max(-50, min(50, turnSpeed))
 
 # PID controller
 def calculateSpeedAndRotation(distanceFromTarget, angleToTarget, state):
@@ -14,7 +21,10 @@ def calculateSpeedAndRotation(distanceFromTarget, angleToTarget, state):
 
         goalDistanceFromBall = 20
         # Assuming that we use MoveSteering().on(steering, speed), the values range from -100 to 100, adjust below values accordingly
-        forwardSpeed = max(5, min(100, kp_speed * (distanceFromTarget - goalDistanceFromBall)))
+        
+        # Try limiting forwardSpeed as it determines the speed at which we turn. Maybe especially limit it at large angles? And no limit or lower limit when angle is small enough? 
+        # Try not moving forward until angle is satisfyingly small? Probably slow, but probably effective.
+        forwardSpeed = max(10, min(100, kp_speed * (distanceFromTarget - goalDistanceFromBall)))
         turnSpeed = getTurnSpeed(angleToTarget) 
 
     elif state == "TO_INTERMEDIARY":
