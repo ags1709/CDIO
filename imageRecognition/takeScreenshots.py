8744fa3c491de0db61146f073fc06f2ebda4d6df
Anders Greve Sørensen <anders1709@gmail.com>
2025-06-09 11:17:06 +0200

    Take more pictures

--- imageRecognition/takeScreenshots.py
+++ imageRecognition/takeScreenshots.py
@@ -13,7 +13,7 @@ if not cap.isOpened():
 # today = date.today()
 imgCounter = 0
 frameCounter = 0
-batch = 4
+batch = 5
 while True:
     ret, frame = cap.read()
     # Capture frame-by-frame
@@ -41,7 +41,7 @@ while True:
 
     elif key & 0xFF == ord('s'):
         # imgName = f"customData/{date.today}_picture_{imgCounter}.png"
-        imgName = f"customData/eggSet/batch{batch}_picture{imgCounter}.png"
+        imgName = f"customData/dataset4/batch{batch}_picture{imgCounter}.png"
 
         cv2.imwrite(imgName, frame)
         print("Screenshot taken")
