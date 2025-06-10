886d3a5c033d8fe52de37886b3e1813e4091d933
Anders Greve Sørensen <anders1709@gmail.com>
2025-06-10 08:18:15 +0200

    Change names of dataset file and split them up properly

--- imageRecognition/customData/split.py
+++ imageRecognition/customData/split.py
@@ -3,8 +3,8 @@ import random
 import shutil
 
 # Source directories
-image_dir = './images'  # original image folder
-label_dir = './labels'  # original label folder
+image_dir = './eggSet/images'  # original image folder
+label_dir = './eggSet/labels'  # original label folder
 
 # Output dataset directory
 output_dir = './dataset'
