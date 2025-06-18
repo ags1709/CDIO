34be5fb5d206c329415a1b4acff7e3546f04d6a1
Simon Olsen <s235106@dtu.dk>
2025-06-19 00:06:44 +0200

    Fix it so that we now get the contour of the cross with the new model

--- imageRecognition/detect.py
+++ imageRecognition/detect.py
@@ -80,7 +80,8 @@ class ObjectDetection():
         frontRightCorner = None
         frontLeftCorner = None
         backLeftCorner = None
-        crossinfo = estimateCross(result, cap, frame)
+        cross_id = [k for k, v in self.model.names.items() if v == "cross"][0]
+        crossinfo = estimateCross(result, cap, frame, cross_id)
         playarea = estimatePlayArea(result, cap, frame)
         goals = estimateGoals(playarea, cap, frame)
         playAreaIntermediate = estimatePlayAreaIntermediate(result, playarea, frame) #pa_tl = playarea of top left... etc
@@ -99,6 +100,7 @@ class ObjectDetection():
             # Draw label
             cv2.putText(frame, label, (x1, y1 - 10),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
+            print(self.model.names)
             
             # Save position or box (Depending on which is appropriate) of all detected things
             if cls_id == 0:
@@ -107,20 +109,20 @@ class ObjectDetection():
                 orangeBalls.append(estimatePositionFromSquare(x1, y1, x2, y2))
             elif cls_id == 2:
                 egg = ((x1, y1), (x2, y2))
+            # elif cls_id == 3:
+            #     playfield = ((x1,y1), (x2, y2))
             elif cls_id == 3:
-                playfield = ((x1,y1), (x2, y2))
-            elif cls_id == 4:
                 cross = ((x1, y1), (x2, y2))
-            elif cls_id == 5:
+            elif cls_id == 4:
                 backRightCorner = estimatePositionFromSquare(x1, y1, x2, y2)
                 cv2.circle(frame, tuple_toint(correctPerspective(backRightCorner)), 10, (0,0,255), 10)
-            elif cls_id == 6:
+            elif cls_id == 5:
                 frontRightCorner = estimatePositionFromSquare(x1, y1, x2, y2)
                 cv2.circle(frame, tuple_toint(correctPerspective(frontRightCorner)), 10, (0,0,255), 10)
-            elif cls_id == 7:
+            elif cls_id == 6:
                 frontLeftCorner = estimatePositionFromSquare(x1, y1, x2, y2)
                 cv2.circle(frame, tuple_toint(correctPerspective(frontLeftCorner)), 10, (0,0,255), 10)
-            elif cls_id == 8:
+            elif cls_id == 7:
                 backLeftCorner = estimatePositionFromSquare(x1, y1, x2, y2)
                 cv2.circle(frame, tuple_toint(correctPerspective(backLeftCorner)), 10, (0,0,255), 10)
 
@@ -129,7 +131,9 @@ class ObjectDetection():
         # A dictionary mapping names of objects we want to a list of their positions, each position being a tuple with 2 points
         # The points being respectively the upperleft and bottomright corner of their bounding box. Each point is itself a tuple of 2 integers.
         # NOTE: goals are stored differently to everything else. goals are stored as a tuple with its x coordinate, and the y coordinate being the middle of the goal.
-        positions = {"whiteBalls": whiteBalls, "orangeBalls": orangeBalls,"playfield": playfield, "cross": cross, "egg": egg, "frontLeftCorner": frontLeftCorner, \
+        # positions = {"whiteBalls": whiteBalls, "orangeBalls": orangeBalls,"playfield": playfield, "cross": cross, "egg": egg, "frontLeftCorner": frontLeftCorner, \
+        #      "frontRightCorner": frontRightCorner, "backLeftCorner": backLeftCorner, "backRightCorner": backRightCorner, "goals": goals}
+        positions = {"whiteBalls": whiteBalls, "orangeBalls": orangeBalls, "cross": cross, "egg": egg, "frontLeftCorner": frontLeftCorner, \
                      "frontRightCorner": frontRightCorner, "backLeftCorner": backLeftCorner, "backRightCorner": backRightCorner, "goals": goals}
         
         # Filter robot position
